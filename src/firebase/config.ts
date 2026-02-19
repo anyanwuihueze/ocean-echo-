3e9ac3acb2ca16f5b454b1a79da6859e374c7a73
DIBIA AI <dibia.aiagency@gmail.com>
2026-02-19 16:35:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9675127531-f0451",
+  "appId": "1:1098179171359:web:da42633d94d2011dcc712a",
+  "apiKey": "AIzaSyBQkaJAaZC_eUiW8Wkst2ZYX2e49mHdNQc",
+  "authDomain": "studio-9675127531-f0451.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1098179171359"
+};
